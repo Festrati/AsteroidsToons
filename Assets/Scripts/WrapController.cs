bd496e4b34c7b4904a1b57702127b4fd342f8115
festrati <festrati@gmail.com>
2015-04-10 22:16:03 -0300

    Need draw player x Axis

--- Assets/Scripts/WrapController.cs
+++ Assets/Scripts/WrapController.cs
@@ -7,18 +7,17 @@ public class WrapController : MonoBehaviour {
 	void OnBecameInvisible() 
 	{
 		Debug.Log (" Out of screen!" );
+
 		var cam = Camera.main;
 		var viewportPosition = cam.WorldToViewportPoint (transform.position);
 		var newPosition = transform.position;
 		
-		if (viewportPosition.x > 1 || viewportPosition.x < 0)
-		{
+		if (viewportPosition.x > 1 || viewportPosition.x < 0) {
 			print("TODO: draw!!!!! X ");
 			newPosition.y = -newPosition.y;
 		}
 		
-		if (viewportPosition.y > 1 || viewportPosition.y < 0)
-		{
+		if (viewportPosition.y > 1 || viewportPosition.y < 0) {
 			print("TODO: Draw!!!!! Y ");
 			newPosition.y = -newPosition.y;
 		}
